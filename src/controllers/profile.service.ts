7e3a1f7c938bda77892c12a18ad959789652b861
Awwal <almajiri.dev@gmail.com>
2023-11-13 09:56:36 +0100

    comment out profile service codes

--- src/controllers/profile.service.ts
+++ src/controllers/profile.service.ts
@@ -1,51 +1,51 @@
-import { NextFunction, Request, Response } from "express"
-import { asyncErrorMiddleware } from "../middlewares"
-import { MemberProfileData } from "../types/app"
-import { prisma } from "../lib/db"
+// import { NextFunction, Request, Response } from "express"
+// import { asyncErrorMiddleware } from "../middlewares"
+// import { MemberProfileData } from "../types/app"
+// import { prisma } from "../lib/db"
 
-/**
- * Member
- */
-export const createMemberProfile = asyncErrorMiddleware(
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const {
-        firstName,
-        lastName,
-        otherNames,
-        bio,
-        gender,
-        institution,
-        course,
-        occupation,
-        graduationStatus,
-        graduationDate,
-        stateOfOrigin,
-        stateOfResidence,
-        phoneNumber,
-        email,
-        avatarUrl,
-      }: MemberProfileData = req.body
+// /**
+//  * Member
+//  */
+// export const createMemberProfile = asyncErrorMiddleware(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const {
+//         firstName,
+//         lastName,
+//         otherNames,
+//         bio,
+//         gender,
+//         institution,
+//         course,
+//         occupation,
+//         graduationStatus,
+//         graduationDate,
+//         stateOfOrigin,
+//         stateOfResidence,
+//         phoneNumber,
+//         email,
+//         avatarUrl,
+//       }: MemberProfileData = req.body
 
-      const newMemberProfile = await prisma.memberProfile.create({
-        data: {
-          firstName,
-          lastName,
-          otherNames,
-          bio,
-          gender,
-          institution,
-          course,
-          occupation,
-          graduationStatus,
-          graduationDate,
-          stateOfOrigin,
-          stateOfResidence,
-          phoneNumber,
-          email,
-          avatarUrl,
-        },
-      })
-    } catch (error: any) {}
-  }
-)
+//       const newMemberProfile = await prisma.memberProfile.create({
+//         data: {
+//           firstName,
+//           lastName,
+//           otherNames,
+//           bio,
+//           gender,
+//           institution,
+//           course,
+//           occupation,
+//           graduationStatus,
+//           graduationDate,
+//           stateOfOrigin,
+//           stateOfResidence,
+//           phoneNumber,
+//           email,
+//           avatarUrl,
+//         },
+//       })
+//     } catch (error: any) {}
+//   }
+// )
