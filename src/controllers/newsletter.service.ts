8a44dcb09e9467d9d1a05201f1d8372e14bda4dc
Ridwan Adefemi <121475885+omarCodez@users.noreply.github.com>
2023-12-02 18:51:43 +0000

    schema migrations added nd deployed

--- src/controllers/newsletter.service.ts
+++ src/controllers/newsletter.service.ts
@@ -1,10 +1,18 @@
-import { NextFunction, Response } from "express";
-import { asyncErrorMiddleware } from "../middlewares";
+import { NextFunction, Response } from "express"
+import { asyncErrorMiddleware } from "../middlewares"
+import { ErrorHandler } from "../utils"
+import { prisma } from "../lib/db"
 
-export const subscribeToNewsletter = asyncErrorMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+export const subscribeToNewsletter = asyncErrorMiddleware(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const { email }: { email: string } = req.body
+
+        // const alreadySubscribed = await prisma
+
         
     } catch (error: any) {
-        
+        return next(new ErrorHandler(error.message, 400))
     }
-})
+  }
+)
