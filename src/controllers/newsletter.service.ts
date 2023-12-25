003f010025586dc98a96160f5f00ee5da6bc064d
Awwal Adewuyi <almajiri.dev@gmail.com>
2023-12-25 20:53:22 +0100

    newletter deploy again

--- src/controllers/newsletter.service.ts
+++ src/controllers/newsletter.service.ts
@@ -8,7 +8,7 @@ export const subscribeToNewsletter = asyncErrorMiddleware(
     try {
       const { email }: { email: string } = req.body;
 
-      const alreadySubscribed = await prisma.newsletter.findUnique({
+      const alreadySubscribed = await prisma.newsletter.findFirst({
         where: {
           email,
         },
